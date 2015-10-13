7304186dd328b90ea9f31e605976b49e8d2722f6
lillepersdatter <lillepersdatter>
2015-10-13 22:43:33 +0200

    tuned version

--- scrap.py
+++ scrap.py
@@ -1,45 +1,96 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 12 22:07:14 2015
-
-@author: cp
-"""
-
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+#from urllib.parse import urlparse
+from urlparse import urlparse
+from itertools import groupby, chain
+from datetime import datetime
+import re
+import click
 
-inputfile = open("urlfile.txt", 'rb')
-data = inputfile.read() #read file
-URLS = filter(None, data.splitlines()) #split on lines and remove white spaces
-    
 
-# Retrieve a single page and report the url and contents
-def load_url(url, timeout):
-    session = requests.Session()
-    resp = session.get(url, timeout=timeout)
+#Returns the response given an url
+def load_url(url, timeout=30, session=None, **kwargs):
+    #test if session is given, if yes we do not want to spend time opening a new
+    if session is None:
+        session = requests.Session()
+    resp = session.get(url, timeout=timeout, **kwargs)
     resp.raise_for_status()
     return resp
 
-# We can use a with statement to ensure threads are cleaned up promptly
-with ThreadPoolExecutor(max_workers=5) as executor:
-    # Start the load operations and mark each future with its URL
-    future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
-    
-    for future in as_completed(future_to_url):
-        url = future_to_url[future]
-        try:
-            data = future.result()
-        except Exception as exc:
-            print('\n %r generated an exception: %s' % (url, exc))
+
+#This function parallellize calls to load_url() for each url in a group with same hostnames
+#yields tuple (url, response) or (url, error)
+def _get_batch(urls, threads=6, **kwargs):
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        #make dictionary future: url
+        future_to_url = {executor.submit(load_url, url, **kwargs):
+                         url for url in urls}
+        for future in as_completed(future_to_url):
+            try:
+                yield (future_to_url[future], future.result())
+            except Exception as exc:
+                yield (future_to_url[future], exc)
+
+
+#This function yields lists of urls with similar hostname
+#Input: list of abritatry urls
+def _group_on_hostname(urls):
+    urls = [urlparse(url) for url in urls]
+    urls = sorted(urls, key=lambda o: o.hostname)
+    for k, g in groupby(urls, key=lambda o: o.hostname):
+        yield [o.geturl() for o in g]
+
+
+#This function parallellize calls to _get_batch() for each hostname group of urls
+#Returns a iterable (chain) containing the tuples (url, response) yielded from _get_batch
+def _get_urls(urls, **kwargs):
+    res = []
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        #make dictionary future: host group
+        future_to_group = {executor.submit(_get_batch, group, session=requests.Session(), **kwargs): 
+                           group for group in _group_on_hostname(urls)}
+        for future in as_completed(future_to_group):
+            res = chain(res, future.result()) 
+    return res
+
+#Write reponse status to the log file saved
+def _write_to_log(url, o, **kwargs):
+    with open('log.txt', 'a') as log:
+        if isinstance(o, Exception):
+            log.write('Error: {} generated an exception: {}\n'.format(url, o))
         else:
-            print('\n%r page is %d bytes' % (url, len(data.content)))
+            _safe_file(o, **kwargs)
+            log.write('Saved:{} page ({} bytes)\n'.format(url, len(o.content)))
             #test if content type is an image, print a warning if not
-            if data.headers['content-type'].split('/')[0] != 'image':
-                print("Warning: Content type is not an image. Content type: %r" % (data.headers['content-type']))
+            if o.headers['content-type'].split('/')[0] != 'image':
+                log.write("WARNING: {} is NOT identified as an image. Content type: {}\n".format(url, o.headers['content-type']))
+
+#save image to hard drive            
+def _safe_file(o,count,n_urls):
+    name = str(count).zfill(n_urls) 
+    ext = '.' + list(filter(None, re.split("[/;]+", o.headers['content-type'])))[1]
+    with open(name+ext, 'wb') as download:
+                download.write(o.content)    
                 
-#todo:                
-#number of succesfully loads
-#hereof X number of not an image warnings
-#number of unsuccesfully loads      
-#load images
-#logfile                
+            
+
+
+@click.command()
+@click.argument('inputfile', type=click.File('rb'))
+
+def run(inputfile):
+    """This script downloads images from an input textfile containing urls to the local hard drive.
+    It also saves a log.txt file containing the status for each url. Dovnloads and log.txt are saved to the same location as 
+    this file."""
+    data = inputfile.read() 
+    urls = filter(None, data.splitlines()) #split on lines and remove white spaces
+    count = 1
+    with open('log.txt', 'w') as log:
+        log.write('Logfile created: {} \n\n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    for r in _get_urls(urls, timeout=2):
+        _write_to_log(*r, count=count, n_urls=5)
+        count = count + 1
+
+if __name__ == "__main__":
+    run()
+    
